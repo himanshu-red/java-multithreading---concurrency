f682844c32ed4ba19fb49902e6d3c4693c7979c9
Himanshu <himanshutdw98@gmail.com>
2023-03-29 19:21:38 +0530

    Given a name to thread1

--- com/project/multithreading/Thread1.java
+++ com/project/multithreading/Thread1.java
@@ -1,11 +1,15 @@
 package com.project.multithreading;
 
-public class Thread1 extends Thread{
+public class Thread1 extends Thread {
+
+    public Thread1(String threadName) {
+        super(threadName);
+    }
 
     @Override
-    public void run(){
-        for (int i = 0; i < 5; i++){
-            System.out.println("Inside Thread run " + i);
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Inside Thread : " + currentThread().getName() + " " + i);
         }
     }
 }
--- com/project/multithreading/ThreadTester.java
+++ com/project/multithreading/ThreadTester.java
@@ -4,7 +4,7 @@ public class ThreadTester {
     public static void main(String[] args) {
         System.out.println("Main thread is starting");
 
-        Thread1 thread1 = new Thread1();
+        Thread1 thread1 = new Thread1("Thread-1");
         thread1.setDaemon(true);
         thread1.start();
 
